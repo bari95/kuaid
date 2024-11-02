5141d1aa1bec0e63b4fef0a4901c6158ff0ff996
bari95 <elibarikaneno@gmail.com>
2024-11-02 21:13:30 +0300

    footer bg

---
+++ src/components/Map.tsx
@@ -0,0 +1,29 @@
+
+
+
+import React from 'react';
+
+const MapEmbed = () => {
+  return (
+    <div className="w-full md:w-1/2 px-4 h-96">
+      <div className="h-full w-full bg-gray-200 rounded-lg overflow-hidden shadow-lg border-4 border-blue-500 relative">
+        {/* Header */}
+        <div className="absolute top-0 left-0 w-full bg-blue-600 bg-opacity-90 text-white text-lg font-semibold p-2 rounded-t-lg z-10 shadow-md">
+          <h3 className="text-center">Our Location in Magomeni, Dar es Salaam, Tanzania</h3>
+        </div>
+
+        {/* Map Iframe */}
+        <iframe
+          className="w-full h-full rounded-lg"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.6422920300884!2d39.24752927590747!3d-6.81235006692479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4b38e32b715f%3A0xf08d1375e7d377b6!2sMagomeni%2C%20Dar%20es%20Salaam%2C%20Tanzania!5e0!3m2!1sen!2sus!4v1603794290143!5m2!1sen!2sus"
+          allowFullScreen
+          aria-hidden="false"
+          tabIndex={0}
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default MapEmbed;
